1bd0f38728ab5153b1d3d697d00b24b92c197ee9
FFACode <84370323+FFACode@users.noreply.github.com>
2021-05-18 12:48:11 +0400

    Update FFACode.java

--- Java/FFACode.java
+++ Java/FFACode.java
@@ -1,6 +1,14 @@
+// TM FFACode Team, Contact: ffacode@gmail.com or ffacode@outlook.com .
+// Offical Java FFACode file.
+// For more info contact us.
+package Java.Code;
 public class FFACode
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+        String HelloWorld = "Welcome to World!";
+		System.out.println(HelloWorld);
+		float ErrorNum = 103.2f;
+		float SuccessNum = 205.3f;
+		System.out.println(ErrorNum + SuccessNum);
 	}
 }
